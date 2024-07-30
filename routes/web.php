c130b14a271382285462aac100e03c7858236212
martin-nandor-csikos <csikos.martin17@gmail.com>
2024-07-30 19:49:58 +0200

    added auth namespace

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ReportController;
