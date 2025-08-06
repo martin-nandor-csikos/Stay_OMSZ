5bea3c007369e60f00e24b0c2aef8a1c3a21d821
martin-nandor-csikos <csikos.martin17@gmail.com>
2025-08-06 11:21:38 +0000

    changed text

--- resources/views/inactivity/create_inactivity.blade.php
+++ resources/views/inactivity/create_inactivity.blade.php
@@ -15,7 +15,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 create-report">
             <div class="bg-gray-50 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <p style="text-align: center;">Az inaktivitási kérelem akkor lesz érvényes, ha egy admin elfogadta.</p>
+                    <p style="text-align: center;">Az inaktivitási kérelem akkor lesz érvényes, ha egy leader elfogadta.</p>
                     <br>
                     <form method="POST" action="{{ route('inactivity.store') }}">
                         @csrf
