95718608ea93e8357f0913acaaf387ea21764243
martin-nandor-csikos <csikos.martin17@gmail.com>
2025-08-06 11:20:03 +0000

    created prices table

---
+++ database/factories/PriceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PriceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'diagnosis_name' => fake()->text(),
+            'price' => fake()->randomDigitNotNull(),
+        ];
+    }
+}
